22d5ec22f9306852d5a4db93e7d1824451e03a55
张景赫 <venusxk6214@foxmail.com>
2023-04-04 16:03:21 +0800

    init index.html

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+      'sm': {'max': '1000px'},
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+
+      'md': {'min': '1000px', 'max': '1200px'},
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      'lg': {'min': '1200px', 'max': '1279px'},
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      'xl': {'min': '1280px', 'max': '1535px'},
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+      '2xl': {'min': '1536px'},
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {},
+  }
+}
